fd2c6226819e631de20f85b6c8280501ecc1794e
Rubén <rubenralc@gmail.com>
2018-09-05 14:33:34 +0200

    First commit with first version of the bot

---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+import random
+from discord.ext import commands
+from discord import Embed
+
+bot = commands.Bot(command_prefix='ryf$')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def roll(ctx):
+
+    rollresult = dices()
+
+    mediumresult = sorted(rollresult)[1]
+
+    skill = 7
+    total = mediumresult + skill
+
+    rich=Embed(title="Amedio ha tirado ""Advertir/Notar"" con un resultado de 10")
+    rich.add_field(name="tirada", value=rollresult, inline=True)
+    rich.add_field(name="dado medio", value=mediumresult, inline=True)
+    rich.add_field(name="resultado", value="{0} + {1} = {2}".format(mediumresult, skill, total), inline=False)
+
+    await ctx.send(embed=rich)
+
+def dices():
+    min = 1
+    max = 10
+
+    result = []
+
+    for _ in range(3):
+        result.append(random.randint(min, max))
+
+    return result
+
+
+bot.run('NDg2NTE2OTU1MDgwMDMyMjU3.DnAVeQ.D3i-Xgr715KvME3knB94KF-0Rv8')
