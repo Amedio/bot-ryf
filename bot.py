c0928d4e35609f5e44f37db79922b529b8f4ae1e
Amedio <rubenralc@gmail.com>
2018-09-06 08:51:13 +0200

    adding criticals and critical failures

--- bot.py
+++ bot.py
@@ -21,6 +21,17 @@ async def roll(ctx, *args):
 
     objective = sorted(rollresult)[1]
 
+    if objective == 1:
+        next = sorted(rollresult)[2]
+        if next <= 5:
+            rich=Embed(title="El resultado de la tirada de {0.author.display_name} es **PIFIA**".format(ctx))
+            rich.add_field(name="tirada", value=allrolls, inline=False)
+            rich.add_field(name="dado objetivo", value=objective, inline=True)
+            rich.add_field(name="dado siguiente", value=next, inline=True)
+
+            await ctx.send(embed=rich)
+            return
+
     totalroll = objective
 
     while objective == 10:
@@ -38,6 +49,11 @@ async def roll(ctx, *args):
         rich.add_field(name="tirada", value=allrolls, inline=True)
         rich.add_field(name="dado objetivo", value=totalroll, inline=True)
         rich.add_field(name="resultado", value="{0} + {1} = {2}".format(totalroll, skill, total), inline=False)
+
+        if len(args) > 1:
+            difficulty = int(args[1])
+            if total - difficulty >= 10:
+                rich.add_field(name="CRÍTICO", value="Ha sido crítico", inline=True)
 
         await ctx.send(embed=rich)
     else:
