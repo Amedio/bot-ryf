4eda1ace2dfcd80913933a7ac3af7b4157e0dc4a
Amedio <rubenralc@gmail.com>
2018-09-06 14:32:19 +0200

    args context check

--- checkers.py
+++ checkers.py
@@ -2,6 +2,6 @@ from discord.ext import commands
 
 def to_much_dices():
     async def predicate(ctx):
-        print(ctx)
-        return ctx.args[0] >= 100
+        print(ctx.args)
+        return ctx.args[0] < 100
     return commands.check(predicate)
