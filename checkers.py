8f0a434b37a401f2fac7af093bfc5859a0d28a3f
Amedio <rubenralc@gmail.com>
2018-09-06 14:59:53 +0200

    deleted checkers module

--- checkers.py
+++
@@ -1,7 +0,0 @@
-from discord.ext import commands
-
-def to_much_dices():
-    async def predicate(ctx):
-        print(ctx.args)
-        return ctx.args[0] < 100
-    return commands.check(predicate)
